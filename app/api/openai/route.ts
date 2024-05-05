53e6aee3242c8a6ef8ba6d4f6f6a71b3dfa7cb22
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-05-05 08:39:36 -0400

    add full prompt for chatGPT, successfully return in json

--- app/api/openai/route.ts
+++ app/api/openai/route.ts
@@ -16,13 +16,22 @@ export async function POST(req: Request) {
   }
 
   const formData = await req.formData();
-  const prompt = formData.get("notes");
-  if (!prompt) {
+  const notes = formData.get("notes");
+  if (!notes) {
     return Response.json("No prompt provided", { status: 400 });
   }
   const openAIResponse = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: prompt }],
+    messages: [
+      {
+        role: "user",
+        content: `You are a knowledgeable teacher with years of experience helping students learn complex topics by breaking them down into understanable pieces. Take the following notes (deliniated between '---') and turn them into a reasonable amount of matching questions and answers that the student can use for flashcards to gain a deep understanding. Please only return your response in json format and add a relevant 'subject' key to each question and answer. Here are the notes: \n
+    ---
+    ${notes}
+    ---
+    `,
+      },
+    ],
     max_tokens: 100,
   });
 
