7ec1840c60e5c7e73ca23bc8340e69714189c82f
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-05-05 09:11:35 -0400

    json response working from api route but can't display questions

--- app/api/openai/route.ts
+++ app/api/openai/route.ts
@@ -15,8 +15,11 @@ export async function POST(req: Request) {
     redirect("/login");
   }
 
-  const formData = await req.formData();
-  const notes = formData.get("notes");
+  const notes = await req.body;
+
+  // if submitted directly from form:
+  // const formData = await req.formData();
+  // const notes = formData.get("notes");
   if (!notes) {
     return Response.json("No prompt provided", { status: 400 });
   }
@@ -25,17 +28,23 @@ export async function POST(req: Request) {
     messages: [
       {
         role: "user",
-        content: `You are a knowledgeable teacher with years of experience helping students learn complex topics by breaking them down into understanable pieces. Take the following notes (deliniated between '---') and turn them into a reasonable amount of matching questions and answers that the student can use for flashcards to gain a deep understanding. Please only return your response in json format and add a relevant 'subject' key to each question and answer. Here are the notes: \n
+        content: `You are a knowledgeable teacher with years of experience helping students learn complex topics by breaking them down into understanable pieces. Take the following notes (deliniated between '---') and turn them into a reasonable amount of matching questions and answers that the student can use for flashcards. Please only return your response as a valid json object without line breaks and add a relevant, one-word 'subject' key to each question and answer. Make the subject a higher level category - for example if the question is about a javascript method, the category would be 'programming'. Here are the notes: \n
     ---
     ${notes}
     ---
     `,
       },
     ],
-    max_tokens: 100,
+    max_tokens: 1000,
   });
 
-  return Response.json(openAIResponse.choices[0].message.content, {
+  // need to add a try/catch, pass on the error, as well as check that the response is valid JSON
+
+  const flashCardJSONObject = JSON.parse(
+    openAIResponse.choices[0].message.content
+  );
+
+  return Response.json(flashCardJSONObject, {
     status: 200,
   });
 }
--- app/learn/ai/page.tsx
+++ app/learn/ai/page.tsx
@@ -1,16 +1,34 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import React from "react";
+import { set } from "date-fns";
+import React, { useState } from "react";
 
 export default function AIPage() {
+  const [questions, setQuestions] = useState([]);
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const notes = formData.get("notes") as string;
+    const response = await fetch("/api/openai", {
+      method: "POST",
+      body: JSON.stringify({ notes }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    setQuestions(data.questions);
+    console.log(data.questions);
+  }
   return (
     <div>
       <h1>Create Fragments using OpenAI</h1>
       <form
-        action="/api/openai"
-        method="POST"
+        onSubmit={handleSubmit}
         className="min-w-2xl rounded flex flex-col items-left gap-2 max-w-2xl text-zinc-950 mt-10"
       >
         <Label className="text-zinc-300" htmlFor="notes">
@@ -21,6 +39,12 @@ export default function AIPage() {
           Create Fragments
         </Button>
       </form>
+      {questions.length > 0 &&
+        questions.map((question) => (
+          <div key={question} className="mt-5">
+            <h2>{question.subject}</h2>
+          </div>
+        ))}
     </div>
   );
 }
