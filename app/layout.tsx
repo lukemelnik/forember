5571bfbc10ad83bd14458653048ecb666bf9b7e6
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-05-06 15:59:48 -0400

    change app metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Forember",
+  title: "ReCollect",
   description: "The app that makes your memory invincible",
 };
 
