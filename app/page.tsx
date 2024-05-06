ddb38516524b569dc2283fa093de6fe6375b8cef
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-05-06 14:52:28 -0400

    add buttons to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default async function HomePage() {
-  return <div>HOME</div>;
+  return (
+    <div className="flex items-center justify-center min-h-screen gap-4">
+      <Link href="/login">
+        <Button className="text-2xl text-black bg-zinc-300 p-7">Login</Button>
+      </Link>
+      <Link href="/login">
+        <Button className="text-2xl text-black bg-green-500 p-7">
+          Start Learning
+        </Button>
+      </Link>
+    </div>
+  );
 }
