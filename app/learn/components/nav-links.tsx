6ca2b0ed6e5e6bdba6c82b25454fa534f7151f1f
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-05-08 08:08:09 -0400

    fix nav underline location after making nav text size larger

--- app/learn/components/nav-links.tsx
+++ app/learn/components/nav-links.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 export default function NavLinks() {
   const navItemClasses =
-    "relative after:absolute hover:after:bg-zinc-100 content-none after:left-0 after:w-[0%] hover:after:w-[100%] hover:after:top-7 hover:after:h-[2px] after:rounded hover:text-zinc-100 after:duration-500 transition-all";
+    "relative after:absolute hover:after:bg-zinc-100 content-none after:left-0 after:w-[0%] hover:after:w-[100%] hover:after:top-9 hover:after:h-[3px] after:rounded hover:text-zinc-100 after:duration-500 transition-all";
 
   const natItems = [
     { name: "Learn", href: "/learn" },
@@ -17,8 +17,8 @@ export default function NavLinks() {
   ];
 
   return (
-    <div className="pt-10 md:text-2xl text-4xl">
-      <ul className="md:space-y-2 space-y-4">
+    <div className="pt-10  text-3xl">
+      <ul className="space-y-4">
         {natItems.map((item) => (
           <li className="flex gap-1 items-center group" key={item.name}>
             <Link className={navItemClasses} href={item.href}>
