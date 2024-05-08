ef4b8248a627dc57cea233a013c31832911261e5
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-05-08 07:39:46 -0400

    fix zoomed in issue on mobile - the start learning button was set to a fixed size wider than the viewport

--- app/learn/components/practice-dialog.tsx
+++ app/learn/components/practice-dialog.tsx
@@ -57,8 +57,8 @@ export default function PracticeDialog() {
       }}
     >
       <DialogTrigger asChild>
-        <div className="relative z-0 group max-w-max">
-          <Button className="w-96 z-0 bg-zinc-100 text-black  group-hover:scale-105 duration-300 transition-all text-lg p-6">
+        <div className="relative z-0 group max-w-[300px]">
+          <Button className="w-full z-0 bg-zinc-100 text-black  group-hover:scale-105 duration-300 transition-all text-lg p-6">
             Start Learning
           </Button>
           <div className="absolute inset-0 bg-pink-500/70 -z-10  blur-lg scale-105 group-hover:bg-pink-500 group-hover:blur-xl duration-300 group-hover:scale-110"></div>
