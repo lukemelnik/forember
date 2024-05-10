4c7015bcefa814eb61c8abe9d2df7282668d80e2
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-05-09 22:25:29 -0400

    continue styling landing page

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,8 @@ export default async function HomePage() {
             Re<sup>Collect</sup>
           </h1>
         </div>
-        <div className="mr-4">
+        <div className="mr-4 flex gap-3 items-center">
+          {user && <p>Logged in as {user.email}</p>}
           {user ? (
             <Link href="/practice">
               <Button className="text-xl text-black bg-zinc-300 p-5">
@@ -43,18 +44,22 @@ export default async function HomePage() {
           )}
         </div>
       </nav>
-      <main className="mt-40">
-        <section className="flex flex-col items-center ">
-          <div className="text-[95px] text-center -space-y-10 font-bold -rotate-1">
-            <p>Learn faster.</p>
-            <p className="relative z-0 after:absolute after:bg-gradient-to-r after:from-pink-700 after:to-pink-500 after:-left-8 after:-right-8 after:h-24  after:-z-10 after:top-6 text-black">
-              Remember more.
+      <main className="">
+        <div className="absolute m-auto top-[300px] bg-gradient-to-r from-blue-700 to-blue-400 h-72 -rotate-12 w-[800px] rounded-full blur-[100px] opacity-20"></div>
+        <section className="flex flex-col items-center justify-center min-h-screen">
+          <div className="">
+            <div className="text-[95px] flex flex-col items-center -space-y-10 font-bold -rotate-1">
+              <p>Learn faster.</p>
+              <p className="relative z-0 after:absolute after:bg-gradient-to-r after:from-pink-700 after:to-pink-500 after:-left-8 after:-right-8 after:h-24  after:-z-10 after:top-6 text-black bg-opacity-90">
+                Remember more.
+              </p>
+            </div>
+            <p className="text-xl max-w-2xl text-center m-auto">
+              The world is moving at a blinding pace. Upskill quicker and
+              enhance your creativity with the power of AI and spaced
+              repetition.
             </p>
           </div>
-          <p className="text-xl max-w-2xl text-center m-auto">
-            The world is moving at a blinding pace. Upskill quicker and enhance
-            your creativity with the power of AI and spaced repetition.
-          </p>
           <Link href="/login" className="mt-5">
             <Button className="text-2xl text-black bg-zinc-300 p-7">
               Start Now
