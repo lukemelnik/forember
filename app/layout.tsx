99b8d7d07abfcc9517d3041ae9c3251024e0f041
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-05-14 13:21:44 -0400

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "ReCollect",
+  title: "Forember",
   description: "The app that makes your memory invincible",
 };
 
