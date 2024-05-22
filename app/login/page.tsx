56a6f07a55e735578d2e572f2b9c7398b8fd5eb3
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-05-22 14:19:16 -0400

    add information to the login page for using the test account

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -59,63 +59,72 @@ export default function Login({
   };
 
   return (
-    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2 ">
-      <Link
-        href="/"
-        className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background text-white hover:bg-btn-background-hover flex items-center group text-sm"
-      >
-        Back
-      </Link>
+    <>
+      <div className="fixed bottom-0 p-5 border-t-[1px] border-zinc-700 w-full text-center">
+        <p>
+          You can give it a try with the test account using 'test@test.com' and
+          'testing' as the password.
+        </p>
+      </div>
+      <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2 ">
+        <Link
+          href="/"
+          className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground border-white border-2 text-white hover:bg-btn-background-hover flex items-center group text-sm"
+        >
+          Back
+        </Link>
 
-      <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground text-zinc-300">
-        <div className="flex items-center justify-center">
-          <Image
-            src={"/assets/images/favicon.png"}
-            alt={"logo"}
-            width={95}
-            height={95}
+        <form className="animate-in flex flex-col w-full justify-center gap-2 text-foreground text-zinc-300">
+          <div className="flex items-center justify-center">
+            <Image
+              src={"/assets/images/favicon.png"}
+              alt={"logo"}
+              width={95}
+              height={95}
+            />
+            <h1 className="text-white text-5xl pr-8 -ml-3">Forember</h1>
+          </div>
+          <label className="text-md" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="rounded-md px-4 py-2 bg-inherit border mb-6"
+            name="email"
+            placeholder="you@example.com"
+            required
           />
-          <h1 className="text-white text-5xl">Forember</h1>
-        </div>
-        <label className="text-md" htmlFor="email">
-          Email
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          name="email"
-          placeholder="you@example.com"
-          required
-        />
-        <label className="text-md" htmlFor="password">
-          Password
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          type="password"
-          name="password"
-          placeholder="••••••••"
-          required
-        />
-        <SubmitButton
-          formAction={signIn}
-          className="bg-pink-600 rounded-md px-4 py-2 text-foreground mb-2 hover:bg-pink-500"
-          pendingText="Signing In..."
-        >
-          Sign In
-        </SubmitButton>
-        <SubmitButton
-          formAction={signUp}
-          className="border border-foreground/20 rounded-md px-4 py-2 text-black bg-zinc-300 mb-2 hover:bg-zinc-200"
-          pendingText="Signing Up..."
-        >
-          Sign Up
-        </SubmitButton>
-        {searchParams?.message && (
-          <p className="mt-4 p-4 bg-foreground/10 text-white text-center">
-            {searchParams.message}
-          </p>
-        )}
-      </form>
-    </div>
+          <label className="text-md" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="rounded-md px-4 py-2 bg-inherit border mb-6"
+            type="password"
+            name="password"
+            placeholder="••••••••"
+            required
+          />
+
+          <SubmitButton
+            formAction={signIn}
+            className="bg-pink-600 rounded-md px-4 py-2 text-foreground mb-2 hover:bg-pink-500"
+            pendingText="Signing In..."
+          >
+            Sign In
+          </SubmitButton>
+          <SubmitButton
+            formAction={signUp}
+            className="border border-foreground/20 rounded-md px-4 py-2 text-black bg-zinc-300 mb-2 hover:bg-zinc-200"
+            pendingText="Signing Up..."
+          >
+            Sign Up
+          </SubmitButton>
+          {searchParams?.message && (
+            <p className="mt-4 p-4 bg-foreground/10 text-white text-center">
+              {searchParams.message}
+            </p>
+          )}
+        </form>
+      </div>
+    </>
   );
 }
