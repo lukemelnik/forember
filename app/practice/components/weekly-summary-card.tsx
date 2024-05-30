bc46a3ecff1cb4818e406c484308f7b8014317d2
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-05-30 14:52:04 -0400

    add date to dashboard & style

--- app/practice/components/weekly-summary-card.tsx
+++ app/practice/components/weekly-summary-card.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Card,
   CardContent,
@@ -5,7 +7,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import React from "react";
+import React, { useEffect } from "react";
 import { Profile, Session } from "./learning-dashboard";
 
 export default function WeeklySummaryCard({
@@ -15,10 +17,36 @@ export default function WeeklySummaryCard({
   profile: Profile[];
   sessions: Session[];
 }) {
+  const [timeOfDay, setTimeOfDay] = React.useState("");
+
+  useEffect(() => {
+    function getCurrentTime() {
+      const date = new Date();
+      const hour = date.getHours();
+      if (hour >= 12 && hour < 16) {
+        setTimeOfDay("Afternoon");
+      } else if (hour >= 18) {
+        setTimeOfDay("Evening");
+      }
+    }
+    getCurrentTime();
+  });
+
+  const date = new Date().toDateString();
   return (
     <Card className="h-96 w-full">
       <CardHeader>
-        <CardTitle>Good Morning {profile && profile[0].first_name}</CardTitle>
+        <CardTitle>
+          <div className="flex justify-between items-center">
+            <h1>
+              {timeOfDay &&
+                profile &&
+                `Good ${timeOfDay} ${profile[0].first_name}`}
+            </h1>
+            <p>{date}</p>
+          </div>
+          <p className="pt-2">It's time to shred.</p>
+        </CardTitle>
       </CardHeader>
       <CardContent>
         <p className="font-black text-4xl">CONTENT</p>
