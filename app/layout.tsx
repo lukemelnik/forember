b8601ad5610d8bc29e72af62508ad8f1dd0633a5
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-05-30 13:33:42 -0400

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 import { Toaster } from "sonner";
+import { Analytics } from "@vercel/analytics/react";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -18,13 +19,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={GeistSans.className}>
-      <body className="bg-black">
-        <main className="min-h-svh flex flex-col items-center text-zinc-300">
-          {children}
-        </main>
-        <Toaster />
-      </body>
-    </html>
+    <>
+      <Analytics />
+      <html lang="en" className={GeistSans.className}>
+        <body className="bg-black">
+          <main className="min-h-svh flex flex-col items-center text-zinc-300">
+            {children}
+          </main>
+          <Toaster />
+        </body>
+      </html>
+    </>
   );
 }
