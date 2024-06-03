890f99af81119d84dfa50598e35330ff9d630f12
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-06-03 11:59:21 -0400

    refactor quiz component

---
+++ app/practice/components/quiz/quiz-finished.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Confetti from "./confetti";
+import { TestScore } from "./practice-dialog";
+
+export default function QuizFinished({ testScore }: { testScore: TestScore }) {
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <div className="fixed z-50 top-0 inset-0 h-screen">
+          <Confetti />
+        </div>,
+        document.body
+      )}
+      <div className="text-zinc-300">
+        <h1 className="mb-5">All done! Here's your score:</h1>
+        <p>
+          You answered {testScore.right} out of{" "}
+          {testScore.right + testScore.wrong} correctly. That's an average of{" "}
+          {Math.round(
+            (testScore.right / (testScore.right + testScore.wrong)) * 100
+          )}
+          %.{" "}
+          <p className="font-bold text-lg">
+            {testScore.right > testScore.wrong
+              ? "Great job!"
+              : "Keep practicing!"}
+            {/* in the future could add links to resources for improving */}
+          </p>
+        </p>
+      </div>
+    </>
+  );
+}
--- app/practice/components/quiz/quiz.tsx
+++ app/practice/components/quiz/quiz.tsx
@@ -7,6 +7,7 @@ import Confetti from "./confetti";
 import ReactDOM from "react-dom";
 import { Progress } from "@/components/ui/progress";
 import { TestScore } from "./practice-dialog";
+import QuizFinished from "./quiz-finished";
 
 export type Fragment = {
   id: string;
@@ -83,37 +84,10 @@ export default function Quiz({
     setFragments(newFragments);
   }
 
-  if (quizOver)
-    return (
-      <>
-        {ReactDOM.createPortal(
-          <div className="fixed z-50 top-0 inset-0 h-screen">
-            <Confetti />
-          </div>,
-          document.body
-        )}
-        <div>
-          <h1 className="mb-5">All done! Here's your score:</h1>
-          <p>
-            You answered {testScore.right} out of{" "}
-            {testScore.right + testScore.wrong} correctly. That's an average of{" "}
-            {Math.round(
-              (testScore.right / (testScore.right + testScore.wrong)) * 100
-            )}
-            %.{" "}
-            <p className="font-bold text-lg">
-              {testScore.right > testScore.wrong
-                ? "Great job!"
-                : "Keep practicing!"}
-              {/* in the future could add links to resources for improving */}
-            </p>
-          </p>
-        </div>
-      </>
-    );
+  if (quizOver) return <QuizFinished testScore={testScore} />;
 
   return (
-    <div>
+    <div className="text-zinc-300">
       {loading && <p>Loading...</p>}
       {fragments.length === 0 && !loading && (
         <p>You're all done for the day!</p>
