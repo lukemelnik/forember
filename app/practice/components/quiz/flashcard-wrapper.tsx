68f7cba6031407698b9f6be436849ece6f64df94
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-06-03 22:36:23 -0400

    fix flashcard wrapper, was missing {children }

--- app/practice/components/quiz/flashcard-wrapper.tsx
+++ app/practice/components/quiz/flashcard-wrapper.tsx
@@ -13,6 +13,8 @@ export default function FlashCardWrapper({
     <div
       className={`w-full h-72  rounded-xl  relative flex justify-center items-center shadow-lg   p-10 text-lg mx-auto from-zinc-100 to-zinc-300 ${isFlipped ? " bg-gradient-to-l" : "bg-gradient-to-r"}`}
       onClick={() => setIsFlipped(!isFlipped)}
-    ></div>
+    >
+      {children}
+    </div>
   );
 }
