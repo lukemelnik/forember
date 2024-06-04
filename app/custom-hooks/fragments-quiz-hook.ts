2e6d6ed14022e62a5fec515a19622f2a497ecf64
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-06-04 12:10:22 -0400

    refactor quiz state into custom hook

---
+++ app/custom-hooks/fragments-quiz-hook.ts
@@ -0,0 +1,79 @@
+"use client";
+
+import { createClient } from "@/utils/supabase/client";
+import { useEffect, useState } from "react";
+import { Fragment } from "../practice/components/quiz/quiz";
+import { isPast, startOfDay } from "date-fns";
+
+export function useFragmentsQuiz() {
+  const [questionNumber, setQuestionNumber] = useState(0);
+  const [quizOver, setQuizOver] = useState(false);
+  const [fragments, setFragments] = useState<Fragment[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function getFragments() {
+      setLoading(true);
+      const supabase = createClient();
+      const {
+        data: { user },
+        error: userError,
+      } = await supabase.auth.getUser();
+      if (!user) {
+        return;
+      }
+      const { data: fragments, error } = await supabase
+        .from("fragment")
+        .select("*")
+        .eq("is_complete", false)
+        .eq("user_id", user.id);
+      if (error) {
+        console.log(error);
+      }
+      if (!fragments) return;
+
+      // conditions for showing the fragment:
+      // 1. if the fragment has never been shown
+      // 2. if the fragment has been shown and the next show date is today
+      // 3. if the fragment has been shown and the next show date is in the past (i.e. user missed a day)
+      // this should actually be moved to the db query, but I'm doing it here for now
+      const filteredFragments = fragments.filter((fragment) => {
+        const fragmentNextShowDay = startOfDay(
+          new Date(fragment.next_show_date)
+        );
+        const today = startOfDay(new Date());
+        return (
+          fragmentNextShowDay.getTime() === today.getTime() ||
+          fragment.last_shown_at === null ||
+          isPast(fragmentNextShowDay)
+        );
+      });
+      setFragments(filteredFragments);
+    }
+    getFragments();
+    setLoading(false);
+  }, []);
+
+  function nextQuestion() {
+    if (questionNumber === fragments.length - 1) {
+      setQuizOver(true);
+      return;
+    }
+    setQuestionNumber(questionNumber + 1);
+  }
+  // separate function for deleting fragment from client view
+  // without it the UI doesn't update, only updates db
+  function deleteFromQuiz(id: string) {
+    const newFragments = fragments.filter((fragment) => fragment.id !== id);
+    setFragments(newFragments);
+  }
+
+  return {
+    fragments,
+    loading,
+    deleteFromQuiz,
+    quizOver,
+    questionNumber,
+    nextQuestion,
+  };
+}
--- app/practice/components/quiz/quiz.tsx
+++ app/practice/components/quiz/quiz.tsx
@@ -2,12 +2,11 @@
 import { createClient } from "@/utils/supabase/client";
 import { useEffect, useState } from "react";
 import FlashCard from "./flash-card";
-import { addDays, isPast, set, startOfDay } from "date-fns";
-import Confetti from "./confetti";
-import ReactDOM from "react-dom";
+import { isPast, startOfDay } from "date-fns";
 import { Progress } from "@/components/ui/progress";
 import { TestScore } from "./quiz-dialog";
 import QuizFinished from "./quiz-finished";
+import { useFragmentsQuiz } from "@/app/custom-hooks/fragments-quiz-hook";
 
 export type Fragment = {
   id: string;
@@ -25,70 +24,18 @@ export default function Quiz({
   handleRightAnswer: () => void;
   handleWrongAnswer: () => void;
 }) {
-  const [questionNumber, setQuestionNumber] = useState(0);
-  const [quizOver, setQuizOver] = useState(false);
-  const [fragments, setFragments] = useState<Fragment[]>([]);
-  const [loading, setLoading] = useState(false);
+  // custom hook that fetchs fragments & manages quiz state
+  const {
+    fragments,
+    loading,
+    deleteFromQuiz,
+    quizOver,
+    questionNumber,
+    nextQuestion,
+  } = useFragmentsQuiz();
 
   // calulate progress for progress bar
   const progress = Math.round((questionNumber / fragments.length) * 100);
-
-  useEffect(() => {
-    async function getFragments() {
-      setLoading(true);
-      const supabase = createClient();
-      const {
-        data: { user },
-        error: userError,
-      } = await supabase.auth.getUser();
-      if (!user) {
-        return;
-      }
-      const { data: fragments, error } = await supabase
-        .from("fragment")
-        .select("*")
-        .eq("is_complete", false)
-        .eq("user_id", user.id);
-      if (error) {
-        console.log(error);
-      }
-      if (!fragments) return;
-
-      // conditions for showing the fragment:
-      // 1. if the fragment has never been shown
-      // 2. if the fragment has been shown and the next show date is today
-      // 3. if the fragment has been shown and the next show date is in the past (i.e. user missed a day)
-      // this should actually be moved to the db query, but I'm doing it here for now
-      const filteredFragments = fragments.filter((fragment) => {
-        const fragmentNextShowDay = startOfDay(
-          new Date(fragment.next_show_date)
-        );
-        const today = startOfDay(new Date());
-        return (
-          fragmentNextShowDay.getTime() === today.getTime() ||
-          fragment.last_shown_at === null ||
-          isPast(fragmentNextShowDay)
-        );
-      });
-      setFragments(filteredFragments);
-    }
-    getFragments();
-    setLoading(false);
-  }, []);
-
-  function nextQuestion() {
-    if (questionNumber === fragments.length - 1) {
-      setQuizOver(true);
-      return;
-    }
-    setQuestionNumber(questionNumber + 1);
-  }
-  // separate function for deleting fragment from client view
-  // without it the UI doesn't update, only updates db
-  function deleteFromQuiz(id: string) {
-    const newFragments = fragments.filter((fragment) => fragment.id !== id);
-    setFragments(newFragments);
-  }
 
   if (quizOver) return <QuizFinished testScore={testScore} />;
 
