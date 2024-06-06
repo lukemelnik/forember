17cd653f57cb9011c54bc0cca5225e86c193037e
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-06-06 14:42:06 -0400

    style loading spinner

--- app/practice/loading.tsx
+++ app/practice/loading.tsx
@@ -2,8 +2,9 @@ import React from "react";
 
 export default function loading() {
   return (
-    <div className="flex min-h-svh items-center justify-center">
-      <div className="flex h-72 w-72 animate-spin items-center justify-center rounded-full border-b-[15px] border-t-[15px] border-zinc-300"></div>
+    <div className="relative flex min-h-svh items-center justify-center">
+      <div className="z-10 flex h-60 w-60 animate-spin items-center justify-center rounded-full border-b-[15px] border-t-[15px] border-zinc-300"></div>
+      <div className="absolute flex h-60 w-60 animate-spin items-center justify-center rounded-full border-b-[15px] border-t-[15px] border-pink-600 blur-lg"></div>
     </div>
   );
 }
